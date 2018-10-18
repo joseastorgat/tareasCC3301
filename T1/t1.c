80580b31c9efb9d48775164c8124587f166148e9
joseastorgat <jose.n.astorga.tobar@gmail.com>
2018-10-18 02:22:18 -0300

    parte c - funcionando

--- T1/t1.c
+++ T1/t1.c
@@ -8,7 +8,6 @@ uint borrar_bits(uint x, uint pat, int len){
 	uint res = x;
 	int i;
 
-	// len == 32? mascara = -1 :  (1 << len) -1;
 	if(len == 32){
 		mascara = -1;
 	}
@@ -23,7 +22,6 @@ uint borrar_bits(uint x, uint pat, int len){
 			res = res & ( ~ mask );  // Para eliminar se niega mas  1111...100...001...1
 		}
 	}
-	printf("%x\n", res);
 	return res;
 }
 
@@ -47,13 +45,10 @@ char *reemplazo(char *s, char c, char *pal){
 
 	s-=largo;
 	char *res = malloc(sizeof(char)*(largo+apariciones*(largo_pal-1)));
-	// printf("%s\n", s);
-	// printf("%s\n", pal);
-	// printf("%d\n", apariciones);
+
 	while(*s!=0){
 		if(*s==c){
 			int j;
-			// printf("%s\n", "hi");
 			for(j=0; j<largo_pal; j++){
 				*res = *pal;
 				res++;
@@ -70,15 +65,80 @@ char *reemplazo(char *s, char c, char *pal){
 	}
 	res-=(largo+apariciones*(largo_pal-1));
 	s-=largo;
-	printf("%s\n",res );
 	return res;
 }
 
 void reemplazar(char *s, char c, char *pal){
+	int largo=0;
+	int largo_pal = 0;
+	int apariciones =0;
+	int i;
+
+	while(*pal!=0){
+		pal++;
+		largo_pal +=1;
+	}
+	pal-=largo_pal;
+	
+	while(*s!=0){
+		if(*s==c){apariciones++;}
+		largo +=1;
+		s++;
+	}
+
+	if(largo_pal >= 1){
+		char *d = s;
+		int i;
+		d += (largo_pal-1)*apariciones;
+		
+		for(i = 0; i < largo; ++i)
+		{
+			if(*s==c){
+				int j;
+				pal+=largo_pal-1;
+				for(j=0; j < largo_pal; j++){
+					*d = *pal;
+					d--;
+					pal--;
+				}
+				d++;
+				pal++;
+			}
+			else{ *d= *s;}
+			s--;
+			d--;
+		}
+	}
+
+	else{
+		s-=largo;
+		for (i = 0; i < largo; i++)
+		{
+			if(*s == c){
+				if (largo_pal>0)
+				{
+					*s = *pal;
+				}
+				else{
+					int j;
+					char *d = s+1;
+					for(j=i; j<largo; j++){
+						*s = *d;
+						s++;
+						d++;
+					}
+					s-=largo-i;
+				}
+			}
+			s++;
+		}	
+	}
+
+
 	return;
 }
 // void main(){
-// 	char *src= "hola que tal";
-//     char *res= reemplazo(src, 'a', "xyz");
-//     printf("%s\n",res );
+// 	char src[25]="%&/(hola que tal\0    )=?";
+//     char *r= &src[4]; 
+//     reemplazar(r, 'a', "opa");
 // }

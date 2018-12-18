3e99e4c1a0036fa67105ccd5cc46eba884ad441a
joseastorgat <jose.n.astorga.tobar@gmail.com>
2018-12-18 02:46:13 -0300

    t4:add comments and some error handling

--- t4/auto.c
+++ t4/auto.c
@@ -7,40 +7,60 @@
 #include "jsocket.h"
 #include "util.h"
 
-
-
 int main(int argc, char **argv){
-
-
 	char tipo   =  *argv[2];
 	char *nombre =  argv[1];
-
-	char l;
+	char l, *conn, *server;
 	long s;
 
-	s = j_socket();
+	if(  !((tipo=='r' && argc ==4 ) || (tipo=='l' && argc==3))){
+		fprintf(stderr, "Argumentos Invalidos \n" );
+		exit(1);
+	}
 
-	if(j_connect(s, "localhost", 2000)<0){
+	if( (conn = getenv("PARKING") ) == 0 ){
+		fprintf(stderr, "Variable PARKING con parametros de conexion NO definida\n");
+		exit(1);
+	}
+
+	//parsing servidor:port de variable PARKING
+	server = conn;
+	while((*conn) != ':'){
+		conn++;
+		if(*conn == 0){
+			fprintf(stderr, "Error en la definicion variable PARKING \n");
+			exit(1);
+		}
+	}
+
+	*conn = 0;
+	conn++;
+
+	int port = atoi(conn);
+	
+	//conexion	
+	s = j_socket(); 
+	if(j_connect(s, server, port)<0){
 		fprintf(stderr, "connection refused");
 		exit(1);
 	}
 
-	write(s, &tipo, 1);  //l o r
+	write(s, &tipo, 1);  // enviar tipo de accion
 	l = strlen(nombre) + 1;
-	write(s, &l, 1); // largo nombre 
-	write(s, nombre, l); // nombre
+	write(s, &l, 1); // enviar largo del nombre 
+	write(s, nombre, l); // enviar nombre
 
 	if(tipo == 'r'){
-		printf("sending tipo\n");
-		char veh =  atoi(argv[3]);		
-		write(s, &veh, 1); //tipo vehiculo
+		int veh	=  atoi(argv[3]);	
+		if(veh<1 || veh>5){
+			fprintf(stderr, "Tipo de vehiculo invalido!\n");
+			exit(1);
+		}
+		write(s, &veh, 1); //enviar tipo vehiculo
+		read(s , &l, 1);
+		printf("%i \n", l );
 	}
 
-	read(s , &l, 1);
-
-	printf("%i \n", l );
-	close(s);
-
+	close(s); //cierre conexion
 	return 0;
-
 }
--- t4/parking.c
+++ t4/parking.c
@@ -7,39 +7,42 @@
 #include "jsocket.h"
 #include "util.h"
 
-char *slots[5] = {0,0,0,0,0};
-int usando = 0;
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t c = PTHREAD_COND_INITIALIZER;
+/* variables globales */
+char *slots[5] = {0,0,0,0,0};  //slots para estacionar
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER; //mutex
+pthread_cond_t c = PTHREAD_COND_INITIALIZER;   //condicion
+typedef void *(*Thread_fun)( void *); 
 
-typedef void *(*Thread_fun)( void *);
-
-
-void print_disponibles(){
-	pthread_mutex_lock(&m);
-	for(int i = 0; i<5; i++){
-
-	if(slots[i]==0){
-		printf("%d",i);
-	}
-	else{
-		printf("*");
+//funcion para imprimir espacios disponibles
+void imprimir_disponibles(){
+	pthread_mutex_lock(&m); // lock para dataraces
+	for(int i = 0; i<5; i++){ //iteramos sobre slots
+		if(slots[i]==0){    // si esta disponible imprime numero slot
+			printf("%d",i);
 		}
+		else{               // sino imprime *
+			printf("*"); 
+			}
 	}
-
-
 	printf("\n");
-	pthread_mutex_unlock(&m);
+	pthread_mutex_unlock(&m); // unlock
 }
 
+
+//funcion para checkear si hay espacio suficiente para estacionar un veh especifico
+// recibe: espacio que ocupa el vehiculo
+// entrega: posicion del primer slot disponible donde pueda estacionar
 int haylugar(int espacio){
+
+	// iterando sobre slots.
 	for(int i = 0; i<5-espacio+1; i++){
-		if(slots[i]==0){
+		
+		if(slots[i]==0){  //slot i esta vacio, ahora comprobar que veh cae en los siguientes slots
 			int result = 1;
-			
-			for(int j=i; j<i+espacio; j++){
-				if(slots[i]!=0){
-					result = 0;
+
+			for(int j=i; j<i+espacio; j++){  
+				if(slots[j]!=0){   //notar que no habra segfault debigo a que j siempre será menor a 4
+					result = 0; 
 					break;
 				}
 			}
@@ -52,9 +55,12 @@ int haylugar(int espacio){
 }
 
 
+// funcion para asignar un estacionamiento
+// retorna int el primer slot desde donde quedo estacionado el auto
 int asignar(char* nombre, char* veh){
-	int lugar;
-	int espacio = (int) *veh;
+	int lugar; //resultado
+	int espacio = (int) *veh; //tipo de vehiculo
+
 	pthread_mutex_lock(&m);
 
 	while((lugar = haylugar(espacio))==-1){
@@ -70,21 +76,23 @@ int asignar(char* nombre, char* veh){
 	return lugar;
 }
 
-
+// funcion para liberar los lugares ocupados por nombre
 void liberar(char* nombre){
-	char* rmnombre;
-	pthread_mutex_lock(&m);
-	for(int i = 0; i<5; i++){
+	char* rmnombre; 
+	
+	pthread_mutex_lock(&m);// lock para evitar dataraces
+	
+	for(int i = 0; i<5; i++){ //buscamos a la persona
 		if(slots[i]!=0){
-			printf("%s\n",slots[i] );
-			if(strcmp(slots[i],nombre) == 0){
+			if(strcmp(slots[i],nombre) == 0){ //desocupamos el slot
 				rmnombre = slots[i];
 				slots[i] = 0;
-		}
+			}
 		}
 	}
-	free(rmnombre);
-	pthread_cond_broadcast(&c);
+
+	free(rmnombre); //liberar memoria ocupada por el nombre
+	pthread_cond_broadcast(&c);// avisamos a quien este esperando su lugar
 	pthread_mutex_unlock(&m);
 }
 
@@ -96,46 +104,55 @@ void *serv( long s){
 	// char* result;
 	char tipo;
 	
-	fprintf(stderr, "cliente conectado\n");
+	// fprintf(stderr, "cliente conectado\n");
 
 	read(s, &tipo, 1);//tipo de consulta
 	read(s, &l, 1);//largo nombre
 	leer(s, nombre, l); //leer nombre
 
-	printf("%s\n",&tipo);
 	if(tipo == 'r'){
 		read(s, &veh, 1); //leer cantidad de puestos
 		char result = asignar(nombre, &veh);
 		write(s, &result, 1);
+		imprimir_disponibles();
+
 	}
-	else{
+	else if(tipo=='l'){
 		liberar(nombre);
+		imprimir_disponibles();
 	}
-	print_disponibles();
+
+	else{
+		char result = -1;
+		write(s, &result, 1);
+
+	}
 	close(s);
 
-	fprintf(stderr, "cliente desconectado\n" );
+	// fprintf(stderr, "cliente desconectado\n" );
 	return NULL;
 
 }
 
 int main(int argc, char **argv){
-
 	long s, s2;
 	pthread_t pid;
 	s = j_socket();
-	// int port = atoi(argv[1]);
-	if(j_bind(s, 2000)<0){
+	
+	int port = atoi(argv[1]);//puerto a utilizar
 
+	if(j_bind(s, port)<0){
 		perror("bind failed");
 		exit(1);
 	}
 
-	print_disponibles();
+	imprimir_disponibles();
 
 	for(;;){
 		pthread_attr_t attr;
-		s2 = j_accept(s);
+		s2 = j_accept(s); // aceptar conexion
+
+		// crear threads auto joinables
 		pthread_attr_init(&attr);
 				
 		if (pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED)!=0){
@@ -146,7 +163,6 @@ int main(int argc, char **argv){
 			perror("no se puede crear thread para nuevo cliente %d");
 
 		}
-
 		pthread_attr_destroy(&attr);
 	}
 	return 0;
